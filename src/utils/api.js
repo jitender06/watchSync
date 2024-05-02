a5e531e699ee98c6c279a7bbfa82b3811c3a326d
jitender <cjatinder39@gmail.com>
2024-05-02 17:19:44 +0530

    init watchsync project on git

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":"df5b7b9822msh8644ff2b6d1ccb7p1f6aeajsna24f0fec03fe",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
